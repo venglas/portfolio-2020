b247649a214e749e4b536c134048a419d9b17003
Mistrz <bartek.bober.koder@gmail.com>
2020-08-11 08:39:43 +0200

    move css call below

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -17,68 +17,64 @@ export default {
     components: {
         Logo
     },
-    created(){
-        console.log(this.$router);
-        
-        
-    }
+    created(){}
 }
 </script>
 
 <style lang="scss" scoped>
-    .router-active-link {
-        &::after {
-            content: '';
-            display: block;
-            margin: -2.5px 0 0 -1rem;
-            height: 2.5px;
-            background: #000;
-            transition: width .3s;
-            animation-name: activeLink;
-            animation-duration: $TIME_fast;
-            animation-timing-function: ease-in-out;
-            animation-fill-mode: forwards;
-        }
-        @keyframes activeLink {
-            from {
-                width: 0%;
-            }
-            to {
-                width: calc(100% + 2rem);
-            }
-        }
-    }
-    .navbar {
-        width: 100%;
-        height: $SIZE_navbar;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        @include light-box-shadow();
+.navbar {
+    width: 100%;
+    height: $SIZE_navbar;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    @include light-box-shadow();
 
-        &__menu {
-            display: flex;
-            width: fit-content;
+    &__menu {
+        display: flex;
+        width: fit-content;
+        height: 100%;
+        list-style-type: none;
+        margin: 0 1rem;
+        .item {
+            padding: 0 2rem;
+            line-height: $SIZE_navbar;
             height: 100%;
-            list-style-type: none;
-            margin: 0 1rem;
-            .item {
-                padding: 0 2rem;
-                line-height: $SIZE_navbar;
-                height: 100%;
-                cursor: pointer;
-                transition: text-shadow ease-in-out $TIME_fast;
-                transition: transform ease-in-out 100ms;
-                outline: none;
-                text-transform: capitalize;
-                @include light-text-shadow();
-                &:hover {
-                    @include light-text-shadow(2.1);
-                }
-                &:active {
-                    transform: scale(.85);
-                }
+            cursor: pointer;
+            transition: text-shadow ease-in-out $TIME_fast;
+            transition: transform ease-in-out 100ms;
+            outline: none;
+            text-transform: capitalize;
+            @include light-text-shadow();
+            &:hover {
+                @include light-text-shadow(2.1);
+            }
+            &:active {
+                transform: scale(.85);
             }
         }
     }
+}
+.router-active-link {
+    &::after {
+        content: '';
+        display: block;
+        margin: -2.5px 0 0 -1rem;
+        height: 2.5px;
+        background: #000;
+        transition: width .3s;
+        animation-name: activeLink;
+        animation-duration: $TIME_fast;
+        animation-timing-function: ease-in-out;
+        animation-fill-mode: forwards;
+    }
+    @keyframes activeLink {
+        from {
+            width: 0%;
+        }
+        to {
+            width: calc(100% + 2rem);
+        }
+    }
+}
 </style>
