f331a2a7bf7cb4b8323df66b672df1137f4a86eb
Bartek <bartek.bober.koder@gmail.com>
2020-09-19 21:15:24 +0200

    add animated background for home route with slot for content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    
-    
+    <Animated-background> 
+      <h1>Elo</h1>
+    </Animated-background>
   </div>
 </template>
 
 <script>
-
-
+import AnimatedBackground from "../components/animated-background/AnimatedBackground";
 
 export default {
   name: 'Home',
   components: {
-
+    "Animated-background": AnimatedBackground
   }
 }
 </script>
