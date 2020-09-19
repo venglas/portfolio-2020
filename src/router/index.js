4f0cd2000a44c82249c5be63fcaf9f98cb794894
Bartek <bartek.bober.koder@gmail.com>
2020-09-19 22:17:03 +0200

    add language siwitcher on beforeEach route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import i18n from '../i18n';
 import routes from "./routes";
 
 Vue.use(VueRouter)
@@ -9,6 +10,14 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   linkExactActiveClass: 'router-active-link',
   routes
-})
+});
+
+//for set page languages
+router.beforeEach((to, from, next) => {
+  let language = to.params.lang;
+  if (!language) language = "pl";
+  i18n.locale = language;
+  next();
+});
 
 export default router
