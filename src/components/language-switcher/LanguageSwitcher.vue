ee8c4a7a5a3ebbd89a6fa81f97ca6acecb536c19
Bartek <bartek.bober.koder@gmail.com>
2020-09-20 13:32:49 +0200

    add changing router param on switching lanugage

--- src/components/language-switcher/LanguageSwitcher.vue
+++ src/components/language-switcher/LanguageSwitcher.vue
@@ -1,9 +1,25 @@
 <template>
-  <select name="language" id="language-switcher" class="language-switcher" v-model="$i18n.locale">
+  <select 
+    name="language" 
+    id="language-switcher" 
+    class="language-switcher" 
+    v-model="$i18n.locale"
+    @change="changeRouterParam()"
+  >
     <option value="pl">PL🇵🇱</option>
     <option value="en">EN🇺🇸</option>
   </select>
 </template>
+
+<script>
+export default {
+    methods: {
+        changeRouterParam() {
+            this.$router.push({params: {lang: this.$i18n.locale}})
+        }
+    }
+}
+</script>
 
 <style lang="scss">
 .language-switcher {
