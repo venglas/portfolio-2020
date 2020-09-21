4c32f7176ec9a711f97863a7fa9b13d59ee5702b
Bartek <bartek.bober.koder@gmail.com>
2020-09-22 00:03:22 +0200

    add base layout for header

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <article class="about">
+    <header class="header">
+      <div class="skill"></div>
+      <div class="skill"></div>
+      <div class="skill"></div>
+      <div class="skill"></div>
+    </header>
+  </article>
 </template>
+
+<style lang="scss">
+  .about {
+    width: 50%;
+    margin: 5rem auto;
+    .header {
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+  .skill {
+    clip-path: polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%);
+    background-color: red;
+    width: 100px;
+    height: 100px;
+  }
+</style>
