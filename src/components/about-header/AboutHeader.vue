8f30ac51fe93070761c41d56aa15b95d07928ec2
Bartek <bartek.bober.koder@gmail.com>
2020-09-22 17:33:14 +0200

    add base content for header

--- src/components/about-header/AboutHeader.vue
+++ src/components/about-header/AboutHeader.vue
@@ -1,26 +1,58 @@
 <template>
     <header class="header">
         <Skill>
-            <template v-slot:img>
+            <template v-slot:skillImg>
                 <img src="../../assets/img/icons/responsive.png" class="img img--skill">
             </template>
+
+            <template v-slot:skillTitle>
+                <h2>Responsywność</h2>
+            </template>
+
+            <template v-slot:skillDescription>
+                Projekty dopasowywuje do wszystkich rozmiarów urządzeń.
+            </template>
         </Skill>
 
         <Skill>
-            <template v-slot:img>
+            <template v-slot:skillImg>
                 <img src="../../assets/img/icons/dynamic.png" class="img img--skill">
             </template>
-        </Skill>
 
-        <Skill>
-            <template v-slot:img>
-                <img src="../../assets/img/icons/clear.png" class="img img--skill">
+            <template v-slot:skillTitle>
+                <h2>Dynamiczność</h2>
+            </template>
+
+            <template v-slot:skillDescription>
+                Strony nie muszą być statyczne! Reaktywne aplikacje webowe to jest to co lubię
             </template>
         </Skill>
 
         <Skill>
-            <template v-slot:img>
+            <template v-slot:skillImg>
+                <img src="../../assets/img/icons/clear.png" class="img img--skill">
+            </template>
+
+            <template v-slot:skillTitle>
+                <h2>Czystość</h2>
+            </template>
+
+            <template v-slot:skillDescription>
+                Kod który wychodzi spod moich rąk jest czysty, pozbawiony bałaganu.
+            </template>
+        </Skill>
+
+        <Skill>
+            <template v-slot:skillImg>
                 <img src="../../assets/img/icons/precise.png" class="img img--skill">
+            </template>
+
+            <template v-slot:skillTitle>
+                <h2>Precyzyjność</h2>
+            </template>
+
+            <template v-slot:skillDescription>
+                Kazde stosowane przeze mnie rozwiązanie staram się wdrazać z pieczołowitością i precyzją.
             </template>
         </Skill>
     </header>
--- src/components/about-header/SingleSkill.vue
+++ src/components/about-header/SingleSkill.vue
@@ -1,33 +1,61 @@
 <template>
     <div class="skill">
-        <slot name="img"/>
+        <div class="skill__img-wrapper">
+            <slot name="skillImg"/>
+        </div>
+        <div class="skill__description">
+            <header class="header">
+                <slot name="skillTitle" />
+            </header>
+            <p class="description">
+                <slot name="skillDescription" />
+            </p>
+        </div>
+        
     </div>
 </template>
 
 <style lang="scss">
 .skill {
-    clip-path: polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%);
-    background-color: $COLOR_about-header-skill-bg;
-    width: 100px;
-    height: 100px;
+    min-width: 250px;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    .img {
-        &--skill {
-            filter: invert(.7);
-            animation-name: skill;
-            animation-duration: 6s;
-            animation-iteration-count: infinite;
-            animation-timing-function: ease;
-        }
 
-        @keyframes skill {
-            0% { filter: invert(0); }
-            40% { filter: invert(1); }
-            80% { filter: invert(1); }
-            100% { filter: invert(0); }
+    &__img-wrapper {
+        clip-path: polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%);
+        background-color: $COLOR_about-header-skill-bg;
+        width: 100px;
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .img {
+            &--skill {
+                filter: invert(.7);
+                animation-name: skill;
+                animation-duration: 6s;
+                animation-iteration-count: infinite;
+                animation-timing-function: ease;
+            }
+
+            @keyframes skill {
+                0% { filter: invert(0); }
+                40% { filter: invert(1); }
+                80% { filter: invert(1); }
+                100% { filter: invert(0); }
+            }
         }
     }
+    &__description {
+        @include space-v-margin-small;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: $SIZE_small-font;
+        text-align: center;
+    }
+    
 }
 </style>
