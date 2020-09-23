5a17ef700620fc25c8adbf4bfe189783c5e2f23a
Bartek <bartek.bober.koder@gmail.com>
2020-09-23 23:59:15 +0200

    add base for about author section

--- src/components/about-content/AboutPresentation.vue
+++ src/components/about-content/AboutPresentation.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
+    <div class="presentation">
         <Octagon-wrapper :width="400" :height="400" padding="4rem">
             <Presentation-animation />
         </Octagon-wrapper>
+        <div class="about-author">
+            <h2>Kim jestem?</h2>
+            <p>
+                Jestem Front-End developerem mieszkającym w Krakowie.
+                Moją pasją jest JavaScript, sport, muzyka i technologie.
+                Z chęcią podejmę z Tobą współpracę, mozemy zbudować ciekawy projekt!
+            </p>
+        </div>
     </div>
 </template>
 
@@ -17,3 +25,9 @@ export default {
     }    
 }
 </script>
+
+<style lang="scss">
+    .presentation {
+        width: 30%;
+    }
+</style>
