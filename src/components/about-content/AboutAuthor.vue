b32e59289efa43fe6d9cefe317f03b945de0e4ff
Bartek <bartek.bober.koder@gmail.com>
2020-09-24 22:17:33 +0200

    move content to another component, add translation

---
+++ src/components/about-content/AboutAuthor.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="about-author">
+    <h2>{{$t('about.presentation.title')}}</h2>
+    <p>{{$t('about.presentation.content')}}</p>
+</div>
+</template>
+
+<script>
+
+export default {
+    components: {}    
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/components/about-content/AboutPresentation.vue
+++ src/components/about-content/AboutPresentation.vue
@@ -3,31 +3,23 @@
         <Octagon-wrapper :width="400" :height="400" padding="4rem">
             <Presentation-animation />
         </Octagon-wrapper>
-        <div class="about-author">
-            <h2>Kim jestem?</h2>
-            <p>
-                Jestem Front-End developerem mieszkającym w Krakowie.
-                Moją pasją jest JavaScript, sport, muzyka i technologie.
-                Z chęcią podejmę z Tobą współpracę, mozemy zbudować ciekawy projekt!
-            </p>
-        </div>
+
+        <About-author />
     </div>
 </template>
 
 <script>
 import OctagonWrapper from "../about-header/OctagonWrapper";
 import PresentationAnimation from "./PresentationAnimation";
+import AboutAuthor from "./AboutAuthor";
 
 export default {
     components: {
         'Octagon-wrapper': OctagonWrapper,
-        'Presentation-animation': PresentationAnimation
+        'Presentation-animation': PresentationAnimation,
+        'About-author': AboutAuthor
     }    
 }
 </script>
 
-<style lang="scss">
-    .presentation {
-        width: 30%;
-    }
-</style>
+<style lang="scss"></style>
