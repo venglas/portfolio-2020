a7ff0257fa0d36af9c6fb00de44bb7347595b4ed
Bartek <bartek.bober.koder@gmail.com>
2020-10-11 19:25:28 +0200

    move companies header into locales

--- src/components/portfolio/CompaniesSection.vue
+++ src/components/portfolio/CompaniesSection.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="companies-section">
-        <h2>Pracowałem równiez dla</h2>
+        <h2>{{$t('portfolio.companiesHeader')}}</h2>
         <div class="companies">
             <Single-company
                 image="lim-logo.png"
