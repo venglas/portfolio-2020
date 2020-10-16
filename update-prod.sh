133202e2b45754498986991764a4c0c5b20e1a67
Bartek <bartek.bober.koder@gmail.com>
2020-10-16 10:29:03 +0200

    update script

--- update-prod.sh
+++ update-prod.sh
@@ -2,7 +2,7 @@ git pull
 npm run build
 cd /var/www/html
 rm -rf *
-cp -rf /root/portfolio-2020/dist/* /var/www/html
+cp -rf /root/projects/portfolio-2020/dist/* /var/www/html
 echo Succesfully updated from branch:
-cd /root/portfolio-2020
+cd /root/projects/portfolio-2020
 git rev-parse --abbrev-ref HEAD
