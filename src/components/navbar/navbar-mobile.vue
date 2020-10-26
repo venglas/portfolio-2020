38a0a02528b67d0271bf0feb09af87ed886df2c6
Bartek <bartek.bober.koder@gmail.com>
2020-10-26 18:16:12 +0100

    refactorize navbar

--- src/components/navbar/navbar-mobile.vue
+++ src/components/navbar/navbar-mobile.vue
@@ -1,19 +1,19 @@
 <template>
-    <transition name="left-slide">
-        <nav
-            class="navbar navbar--mobile"
-            :class="{ 'navbar--mobile-open': getIsMobileMenuOpen }" v-if="!getModalShowState"
-        >
-            <logo />
-            <navbar-links v-if="getIsMobileMenuOpen"/>
-            <hamburger-mobile-menu />
-        </nav>
-    </transition>
+  <transition name="left-slide">
+    <nav
+      class="navbar navbar--mobile"
+      :class="{ 'navbar--mobile-open': getIsMobileMenuOpen }"
+      v-if="!getModalShowState"
+    >
+      <logo />
+      <navbar-links v-if="getIsMobileMenuOpen" />
+      <hamburger-mobile-menu />
+    </nav>
+  </transition>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
-
 import hamburgerMobileMenu from './hamburger-mobile-menu'
 import logo from './logo'
 import navbarLinks from './navbar-links'
@@ -24,7 +24,6 @@ export default {
     logo,
     'navbar-links': navbarLinks
   },
-
   computed: {
     ...mapGetters('app', ['getIsMobileMenuOpen', 'getModalShowState'])
   },
@@ -32,7 +31,7 @@ export default {
     ...mapMutations('app', ['toggleMobileMenu'])
   },
   watch: {
-    '$route' () {
+    $route () {
       this.toggleMobileMenu()
     }
   }
@@ -40,20 +39,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .navbar {
-        display: flex;
-        height: $SIZE_navbar_mobile;
-        max-height: 442.5px;
-        transition: all ease-in-out $TIME_fast;
-        z-index: 999;
-        position: relative;
-        &--mobile {
-            position: sticky;
-            top: 0;
-        }
-        &--mobile-open {
-            flex-direction: column;
-            height: auto;
-        }
-    }
+.navbar {
+  display: flex;
+  height: $SIZE_navbar_mobile;
+  max-height: 442.5px;
+  transition: all ease-in-out $TIME_fast;
+  z-index: 999;
+  position: relative;
+  &--mobile {
+    position: sticky;
+    top: 0;
+  }
+  &--mobile-open {
+    flex-direction: column;
+    height: auto;
+  }
+}
 </style>
--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -29,56 +29,5 @@ export default {
     align-items: center;
     z-index: $SIZE_header-index;
     @include light-box-shadow();
-
-    &__menu {
-        display: flex;
-        width: fit-content;
-        height: 100%;
-        list-style-type: none;
-        margin: 0 1rem;
-        @media (max-width: $MAX_mobile) {
-            font-size: 1.7rem;
-        }
-        @media (max-width: $MEDIUM_mobile) {
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .item {
-            padding: 0 2rem;
-            line-height: $SIZE_navbar;
-            height: 100%;
-            cursor: pointer;
-            transition: text-shadow ease-in-out $TIME_fast-max;
-            transition: transform ease-in-out 100ms;
-            outline: none;
-            text-transform: capitalize;
-            @include light-text-shadow();
-            &:hover {
-                @include light-text-shadow(2.1);
-            }
-            &:active {
-                transform: scale(.85);
-            }
-        }
-    }
-}
-.router-active-link {
-    &::after {
-        content: '';
-        display: block;
-        margin: -2.5px 0 0 -1rem;
-        height: 2.5px;
-        background: #000;
-        transition: width .3s;
-        animation-name: active-link;
-        animation-duration: $TIME_fast-max;
-        animation-timing-function: ease-in-out;
-        animation-fill-mode: forwards;
-    }
-    @keyframes active-link {
-        from { width: 0%; }
-        to { width: calc(100% + 2rem); }
-    }
 }
 </style>
