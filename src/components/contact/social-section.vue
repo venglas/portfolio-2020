edd85a6ac9fbc3cbff91e5e8375acfbf65301505
Bartek <bartek.bober.koder@gmail.com>
2020-10-27 13:26:38 +0100

    add sociales

--- src/components/contact/social-section.vue
+++ src/components/contact/social-section.vue
@@ -1,8 +1,14 @@
 <template>
   <section class="social-section">
     <octagon-wrapper class="social-section__octagon">
-      <a :href="companyLink" target="_blank" >
+      <a href="https://github.com/venglas" target="_blank" >
         <img src="../../assets/img/icons/github.png" class="img" alt="">
+      </a>
+    </octagon-wrapper>
+
+    <octagon-wrapper class="social-section__octagon">
+      <a href="https://www.linkedin.com/in/bartek-bober-71b785151/" target="_blank" >
+        <img src="../../assets/img/icons/linkedin.png" class="img img--linkedin" alt="">
       </a>
     </octagon-wrapper>
   </section>
@@ -23,15 +29,22 @@ export default {
 
 <style lang="scss">
 .social-section {
+  display: flex;
   @include space-v-margin-huge;
   &__octagon {
     width: 10rem;
     height: 10rem;
+    text-align: center;
+    cursor: pointer;
     @include space-padding-small;
+    @include space-h-margin-small;
     .img {
       width: 100%;
       height: auto;
       filter: invert(1);
+      &--linkedin {
+        width: 80%;
+      }
     }
   }
 }
