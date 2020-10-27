ddb9e853df40f41f5cedcbf34e09480c5e064ac1
Bartek <bartek.bober.koder@gmail.com>
2020-10-27 14:15:13 +0100

    admin page base

---
+++ src/components/panel/login-form.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="" class="login-form">
+    <label for="username">username</label>
+    <input type="text" id="username">
+
+    <label for="password">password</label>
+    <input type="password" id="password">
+
+    <input type="submit" value="login">
+  </form>
+</template>
+
+<style lang="scss">
+.login-form {
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    height: 3.5rem;
+    @include space-v-margin-small;
+  }
+}
+</style>
---
+++ src/views/panel.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-article class="article--panel">
+    <login-form />
+  </base-article>
+</template>
+
+<script>
+import LoginForm from '../components/panel/login-form.vue'
+import BaseArticle from '../components/article/base-article.vue'
+
+export default {
+  components: { BaseArticle, LoginForm }
+}
+</script>
+
+<style lang="scss" scoped>
+.article--panel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
