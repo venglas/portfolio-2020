2b1d1625152451169a35aa49f4768589ee0a8c38
Bartek <bartek.bober.koder@gmail.com>
2020-12-10 21:52:20 +0100

    init heroku app

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+mongoose.connect(`${process.env.DB_URL}/portfolio`, { useNewUrlParser: true })
 
 const db = mongoose.connection
 db.on('error', err => console.error(err))
