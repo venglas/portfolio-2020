7433862a60759a1b3711e9b8dc15f6b1264fd49a
Bartek <bartek.bober.koder@gmail.com>
2020-12-18 15:25:08 +0100

    remove throwing errors if cookie not exist

--- src/components/helpers/cookies.js
+++ src/components/helpers/cookies.js
@@ -3,7 +3,8 @@ const isCookieExist = cookieName => {
 }
 
 const getCookieValue = cookieName => {
-  if (!isCookieExist(cookieName)) throw new Error(`Cookie ${cookieName} not exist.`)
+  if (!isCookieExist(cookieName)) return
+  // throw new Error(`Cookie ${cookieName} not exist.`)
   const cookie = document.cookie.split(';').filter(cookie => cookie.includes(cookieName))[0].split('=')[1]
   try {
     return JSON.parse(cookie)
