58028b9397cc671829064b8bb32bbd0a2f4efdf1
Bart <bartek.bober.koder@gmail.com>
2021-03-10 21:56:57 +0100

    test

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const mailRouter = require("./src/routes/mails/mail");
 app.use("/mail", mailRouter);
