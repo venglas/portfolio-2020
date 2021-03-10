7b9602946c2501d4f2a4ead011fe037b0baa409c
Bart <bartek.bober.koder@gmail.com>
2021-03-10 21:17:04 +0100

    clean server

--- server/src/routes/mails/mail.js
+++ server/src/routes/mails/mail.js
@@ -1,45 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const Mail = require("../../models/mail/mail");
-const { checkApplicationID } = require("../../services/mail");
-const { getSession } = require("../../services/session");
+
 const { transporter } = require("../../mailer");
 
-router.get("/", async (req, res) => {
-  // if (await getSession(req.body.id)) {
-  const mails = await Mail.find();
-  res.status(200).json(mails);
-  // }
-  // console.log(mails);
-});
-
 router.post("/", async (req, res) => {
-  if (!checkApplicationID(req.body.applicationID)) {
-    return res.status(401).json({ msg: "application not authorized." });
-  }
-
   const mail = {
     to: "bartek.bober.koder@gmail.com",
     text: `${req.body.message} | from: ${req.body.email}`,
+    html: `<p>${req.body.message}</p> <br /> <p style="border: 2px solid black; border-radius: 4px; padding: 1rem;">${req.body.name} | ${req.body.email}</p>`,
     from: "bartek.bober.koder@gmail.com",
     subject: req.body.subject
   };
 
   transporter.sendMail(mail, (err, info) => {});
-  // const mail = new Mail({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   subject: req.body.subject,
-  //   message: req.body.message,
-  //   applicationID: req.body.applicationID
-  // });
-
-  // try {
-  //   const newMail = await mail.save();
-  //   res.status(201).json(newMail);
-  // } catch (err) {
-  //   res.status(400).json(err.message);
-  // }
 });
 
 module.exports = router;
--- server/src/services/mail.js
+++
@@ -1,6 +0,0 @@
-const checkApplicationID = applicationID => {
-  if (applicationID === process.env.APPLICATION_ID) return true
-  return false
-};
-
-module.exports = { checkApplicationID } 
