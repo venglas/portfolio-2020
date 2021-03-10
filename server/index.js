1b427cefd7e5c574b6a204a10c3fdebd62bc8823
Bart <bartek.bober.koder@gmail.com>
2021-03-10 22:04:54 +0100

    fix cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const mailRouter = require("./src/routes/mails/mail");
+const port = process.env.PORT;
 
-app.use(cors());
+const app = express();
+app.use(cors({ origin: true }));
 app.use(express.json());
 
-const mailRouter = require("./src/routes/mails/mail");
 app.use("/mail", mailRouter);
-
-const port = process.env.PORT;
 
 app.listen(port, () => console.log(`server started at port: ${port}`));
